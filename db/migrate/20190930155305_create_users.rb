cc652d845dbf74bf3961baa58e9aee37195d2aca
Tom White <eugene.white@gmail.com>
2019-09-30 15:53:53 +0000

    Automatically backed up by Learn

--- db/migrate/20190930155305_create_users.rb
+++ db/migrate/20190930155305_create_users.rb
@@ -1,5 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    
+    create_table :dogs do |t|
+      t.string :name
+      t.string :email 
+      t.string :password
+    end
   end
 end
